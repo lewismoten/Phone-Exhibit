b3d6f7b2f32d227df228fb888a082153ddf2ef32
Lewis Moten <lewismoten@gmail.com>
2026-03-27 14:46:34 -0400

    remove vibe coding artifact

--- dashboard.php
+++ dashboard.php
@@ -9,16 +9,15 @@ html_header('Dashboard');
 ?>
 <h1>Dashboard</h1>
 <p>Welcome, <strong><?= e($user['username']) ?></strong>.</p>
-<p>This is where audio submission and management can go next.</p>
-  <ul>
-    <li><a href="audio-files.php">My audio files</a></li>
-    <li><a href="upload-audio.php">Upload audio</a></li>
-    <li><a href="record-audio.php">Record audio</a></li>
-    <li><a href="reserve-number.php">Reserve a Number</a></li>
-    <li><a href="my-numbers.php">My Numbers</a></li>
-    <li><a href="legal.php">Legal Notice</a></li>
-    <li><a href="change-password.php">Change password</a></li>
-    <li><a href="logout.php">Logout</a></li>
+<ul>
+  <li><a href="audio-files.php">My audio files</a></li>
+  <li><a href="upload-audio.php">Upload audio</a></li>
+  <li><a href="record-audio.php">Record audio</a></li>
+  <li><a href="reserve-number.php">Reserve a Number</a></li>
+  <li><a href="my-numbers.php">My Numbers</a></li>
+  <li><a href="legal.php">Legal Notice</a></li>
+  <li><a href="change-password.php">Change password</a></li>
+  <li><a href="logout.php">Logout</a></li>
 </ul>
 <?= get_site_content('dashboard_callout') ?>
 
