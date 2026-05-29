97442d6d2caba614f0ca39df3f6299feff44f680
Lewis Moten <lewismoten@gmail.com>
2026-05-29 02:47:51 -0400

    remove dashboard links already in the navigation

--- dashboard.php
+++ dashboard.php
@@ -11,11 +11,7 @@ html_header('Dashboard');
 <p>Welcome, <strong><?= e($user['username']) ?></strong>.</p>
 <ul>
   <li><a href="audio-files.php">My audio files</a></li>
-  <li><a href="upload-audio.php">Upload audio</a></li>
   <li><a href="record-audio.php">Record audio</a></li>
-  <li><a href="legal.php">Legal Notice</a></li>
-  <li><a href="change-password.php">Change password</a></li>
-  <li><a href="logout.php">Logout</a></li>
 </ul>
 <?= get_site_content('dashboard_callout') ?>
 
